67573d7b35312d8265988a41f4e151ad1a694327
MyNextWeekend <jinhu007@outlook.com>
2024-05-16 21:01:34 +0800

    函数调用例子

--- python/rust_python/__init__.py
+++ python/rust_python/__init__.py
@@ -1,10 +1,12 @@
 from rust_python._lowlevel import (
+    many_args,
     dic_to_list,
     list_to_dic,
     Student
 )
 
 __all__ = [
+    "many_args",
     "dic_to_list",
     "list_to_dic",
     "Student"
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,55 @@
-use pyo3::prelude::*;
+use std::collections::HashMap;
+
+use pyo3::{prelude::*, types::{PyDict, PyTuple, PyList}};
 
 mod other;
 
 use other::student::Student;
 
-/// rust 方法
+// 默认参数以及不定长参数
 #[pyfunction]
-fn dic_to_list() -> PyResult<String> {
-    Ok("Hello from dic_to_list!".into())
+#[pyo3(signature = (num = 10, * py_args, * * py_kwargs))]
+fn many_args(num: i32, py_args: &PyTuple, py_kwargs: Option<&PyDict>) -> PyResult<String> {
+    println!("rust function many_args start");
+    let result = format!(
+        "num: {}  py_args: {:?} py_kwargs: {:?} ",
+        num, py_args, py_kwargs,
+    );
+    Ok(result)
+}
+
+
+#[pyfunction]
+#[pyo3(signature = (input_dic))]
+fn dic_to_list(input_dic: &PyDict) -> PyResult<Vec<&PyAny>> {
+    println!("rust function dic_to_list start");
+
+    let mut result = Vec::new();
+    for (k, v) in input_dic {
+        println!("遍历字典: {k}: {v}");
+        result.push(v)
+    }
+    Ok(result)
 }
 
 #[pyfunction]
-fn list_to_dic() -> PyResult<String> {
-    Ok("Hello from list_to_dic!".into())
+#[pyo3(signature = (names))]
+fn list_to_dic(names: &PyList) -> PyResult<HashMap<usize, &PyAny>> {
+    println!("rust function list_to_dic start");
+
+    let mut result = HashMap::new();
+    for (index, value) in names.iter().enumerate() {
+        println!("遍历列表: {:?}", value);
+        result.insert(index, value);
+    }
+    Ok(result)
 }
 
 
 /// 在Rust中实现的Python模块。
 #[pymodule]
 fn _lowlevel(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(many_args, m)?)?;
     m.add_function(wrap_pyfunction!(dic_to_list, m)?)?;
     m.add_function(wrap_pyfunction!(list_to_dic, m)?)?;
     m.add_class::<Student>()?;
