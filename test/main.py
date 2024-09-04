731739aba863c87ea80c2e9f108e9a8387921fc0
MyNextWeekend <jinhu007@outlook.com>
2024-09-04 20:34:37 +0800

    格式化样式

--- test/main.py
+++ test/main.py
@@ -1,18 +1,16 @@
 import rust_python
-from rust_python import MyError,ChildErrorA
+from rust_python import MyError, ChildErrorA
 
-a_json = {"a":"张三","b":"莉丝","c":"王五"}
-a_list=["张三","赵六","田七","hello"]
+a_json = {"a": "张三", "b": "莉丝", "c": "王五"}
+a_list = ["张三", "赵六", "田七", "hello"]
 
-
-print(f'{"="*20} 函数调用 {"="*20}')
-print(rust_python.many_args(999,*a_list,**a_json))
+print(f'{"=" * 20} 函数调用 {"=" * 20}')
+print(rust_python.many_args(999, *a_list, **a_json))
 print(rust_python.dic_to_list(a_json))
 print(rust_python.list_to_dic(a_list))
 
-
-print(f'{"="*20} 对象调用 {"="*20}')
-student= rust_python.Student("里斯",9)
+print(f'{"=" * 20} 对象调用 {"=" * 20}')
+student = rust_python.Student("里斯", 9)
 print(student)
 print(student.age)
 print(student.name)
@@ -22,6 +20,6 @@ try:
 except ChildErrorA as e:
     print(e)
 
-b_list = [1,2,3,4,50,10]
+b_list = [1, 2, 3, 4, 50, 10]
 print(student.py_set_lages_age(b_list))
 print(student)
