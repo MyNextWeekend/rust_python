96c6281b9156cd850e3f28fb0c5d839796c64ab8
MyNextWeekend <jinhu007@outlook.com>
2024-10-05 15:27:25 +0800

    整理

--- test/demo_thread.py
+++ test/demo_thread.py
@@ -6,17 +6,18 @@ from rust_python import parallel_sum_of_squares
 # 测试多线程耗时
 start = time.time()
 thread_list = []
-for i in range(5):
+for i in range(3):
     t = threading.Thread(target=parallel_sum_of_squares, args=(20000000,))
     t.start()
     thread_list.append(t)
 
 for thread in thread_list:
     thread.join()
-print(f"多线程耗时：{time.time() - start}")
+print(f"多线程耗时：{(time.time() - start):.6f}")
 
 start = time.time()
-parallel_sum_of_squares(20000000)
-parallel_sum_of_squares(20000000)
-parallel_sum_of_squares(20000000)
-print(f"单线程顺序耗时：{time.time() - start}")
+print(parallel_sum_of_squares(20000000))
+print(parallel_sum_of_squares(20000000))
+print(parallel_sum_of_squares(20000000))
+print(f"单线程顺序耗时：{(time.time() - start):.6f}")
+
