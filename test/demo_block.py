8850ef8fbb881f40b2e77f2820b7e39be361dd96
MyNextWeekend <jinhu007@outlook.com>
2024-10-07 20:28:31 +0800

    整理

--- test/demo_block.py
+++ test/demo_block.py
@@ -2,29 +2,30 @@ from rust_python import (many_args, dic_to_list, list_to_dic,
                          student_set_age, student_info,
                          Student,
                          ChildErrorA)
+from log_utils import SingletonLog
 
-a_json = {"a": "张三", "b": "莉丝", "c": "王五"}
-a_list = ["张三", "赵六", "田七", "hello"]
+log = SingletonLog().get_logger()
 
-print(f'{"=" * 20} 函数调用 {"=" * 20}')
-print(many_args(999, *a_list, **a_json))
-print(dic_to_list(a_json))
-print(list_to_dic(a_list))
-
-print(f'{"=" * 20} 对象调用 {"=" * 20}')
+log.info(f"类 方法调用示例{'=' * 20}")
 stu = Student("里斯", 9)
-print(stu)
-print(f"name:{stu.name},age:{stu.age}")
+log.info(f"name:{stu.name},age:{stu.age}")
+b_list = [1, 2, 3, 4, 50, 10]
+stu.py_set_large_age(b_list)
+log.info(f"设置最大值之后: {stu}")
 
+log.info(f"获异常示例{'=' * 20}")
 try:
     stu.raise_exception(1)
 except ChildErrorA as e:
-    print(f"catch except: {e}")
+    log.info(f"catch except: {e}")
 
-b_list = [1, 2, 3, 4, 50, 10]
-stu.py_set_large_age(b_list)
-print(f"设置最大值之后: {stu}")
+log.info(f"函数调用示例{'=' * 20}")
+a_json = {"a": "张三", "b": "莉丝", "c": "王五"}
+a_list = ["张三", "赵六", "田七", "hello"]
+log.info(many_args(999, *a_list, **a_json))
+log.info(f"字典转列表: {dic_to_list(a_json)}")
+log.info(f"列表转字典: {list_to_dic(a_list)}")
 
-# 传入 类实例 作为型参
+log.info(f"函数 操作 类 实例 示例{'=' * 20}")
 student_set_age(stu, 15)
-print(f"通过可变引用修改属性之后: {student_info(stu)}")
+log.info(f"通过可变引用修改属性之后: {student_info(stu)}")
---
+++ test/log_utils.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/16 21:58
+# @Author  : MyNextWeekend
+import functools
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+
+def singleton(cls):
+    """
+    将一个类作为单例
+    来自 https://wiki.python.org/moin/PythonDecoratorLibrary#Singleton
+    """
+
+    cls.__new_original__ = cls.__new__
+
+    @functools.wraps(cls.__new__)
+    def singleton_new(cls, *args, **kw):
+        it = cls.__dict__.get('__it__')
+        if it is not None:
+            return it
+
+        cls.__it__ = it = cls.__new_original__(cls, *args, **kw)
+        it.__init_original__(*args, **kw)
+        return it
+
+    cls.__new__ = singleton_new
+    cls.__init_original__ = cls.__init__
+    cls.__init__ = object.__init__
+
+    return cls
+
+
+@singleton
+class SingletonLog:
+
+    def __init__(self):
+        # 如果已经初始化了就不再执行，避免重复添加handle
+        self.Flag = False
+        self.fmt_str = "%(asctime)s【%(levelname)s】-%(filename)s[%(lineno)d]: %(message)s"
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.DEBUG)
+        # 添加handle
+        self.logger.addHandler(self.console_handle())
+        self.logger.addHandler(self.file_handle())
+
+    def file_handle(self):
+        """日志文件的handle"""
+
+        filename = "./log.log"
+        file_handle = TimedRotatingFileHandler(filename, when='midnight', backupCount=5, encoding='utf-8')
+        file_handle.setLevel(logging.INFO)
+        fmt = logging.Formatter(self.fmt_str)
+        file_handle.setFormatter(fmt)
+        return file_handle
+
+    def console_handle(self):
+        """控制台日志的handle"""
+        console_handle = logging.StreamHandler()
+        console_handle.setLevel(logging.DEBUG)
+        fmt = logging.Formatter(self.fmt_str)
+        console_handle.setFormatter(fmt)
+        return console_handle
+
+    def get_logger(self):
+        return self.logger
