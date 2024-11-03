29e9d1bfc7b64a787ae4f89c21ed6150a4436d86
MyNextWeekend <jinhu007@outlook.com>
2024-11-03 21:54:52 +0800

    refactor: 升级pyo3版本，init文件不采用白名单的形式

--- python/rust_python/__init__.py
+++ python/rust_python/__init__.py
@@ -8,12 +8,12 @@ from rust_python._lowlevel import (
     MyError, ChildErrorA, ChildErrorB, ChildErrorC
 )
 
-__all__ = [
-    "many_args",
-    "dic_to_list",
-    "list_to_dic",
-    "student_info", "student_set_age",
-    "Student",
-    "parallel_sum_of_squares",
-    "MyError", "ChildErrorA", "ChildErrorB", "ChildErrorC"
-]
+# __all__ = [
+#     "many_args",
+#     "dic_to_list",
+#     "list_to_dic",
+#     "student_info", "student_set_age",
+#     "Student",
+#     "parallel_sum_of_squares",
+#     "MyError", "ChildErrorA", "ChildErrorB", "ChildErrorC"
+# ]
