ddb3899f914f579e0e7ef4be7186b817549f771e
MyNextWeekend <jinhu007@outlook.com>
2025-05-25 16:41:33 +0800

    fix: 修复并发计算函数，优化 GIL 释放逻辑以提高性能

--- src/example_thread/thread_case.rs
+++ src/example_thread/thread_case.rs
@@ -1,18 +1,14 @@
 use pyo3::{pyfunction, PyResult, Python};
-use std::thread;
-use std::time::Duration;
 
 /// 利用多线程实现真实的并发
 #[pyfunction]
-pub fn parallel_sum_of_squares(num: usize) -> PyResult<u128> {
-    // 释放 GIL 以便 Rust 能够利用所有 CPU 核心
-    Python::with_gil(|py| py.allow_threads(|| {
-        thread::sleep(Duration::from_secs(3));
+pub fn parallel_sum_of_squares(py: Python<'_>, num: usize) -> PyResult<u128> {
+    py.allow_threads(|| {
         let mut sum: u128 = 0;
         for i in 0..num + 1 {
             let i1 = i as u128;
             sum += i1 * i1;
         }
         Ok(sum)
-    }))
+    })
 }
