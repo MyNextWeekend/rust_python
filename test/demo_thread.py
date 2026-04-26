1bd1b1c916b7bc48686c3d53b5e7114f4c426bfb
MyNextWeekend <jinhu007@outlook.com>
2026-04-26 14:42:41 +0800

    🐞 fix: 修改例子

--- test/demo_thread.py
+++ test/demo_thread.py
@@ -5,23 +5,25 @@ from rust_python import parallel_sum_of_squares
 
 
 def test_thread():
+    number: int = 20000000
+    count: int = 5
+
     # 测试多线程
-    start = time.time()
+    start_time = time.time()
     thread_list = []
-    for i in range(3):
-        t = threading.Thread(target=parallel_sum_of_squares, args=(20000000,))
+    for _ in range(count):
+        t = threading.Thread(target=parallel_sum_of_squares, args=(number,))
         t.start()
         thread_list.append(t)
 
     for thread in thread_list:
         thread.join()
-    print(f"多线程耗时：{(time.time() - start):.6f}")
+    print(f"多线程耗时：{(time.time() - start_time):.6f}")
 
-    start = time.time()
-    print(parallel_sum_of_squares(20000000))
-    print(parallel_sum_of_squares(20000000))
-    print(parallel_sum_of_squares(20000000))
-    print(f"单线程顺序耗时：{(time.time() - start):.6f}")
+    start_time = time.time()
+    for _ in range(count):
+        print(parallel_sum_of_squares(num=number))
+    print(f"单线程顺序耗时：{(time.time() - start_time):.6f}")
 
 
 if __name__ == "__main__":
